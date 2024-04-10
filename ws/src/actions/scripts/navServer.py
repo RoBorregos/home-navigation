40681e6a6cd1aeaf67c360f6f22c47402265079c
Chapa-1810 <alexischaparomero@gmail.com>
2024-04-09 22:42:32 -0600

    Fixed transfrom laser_frame footprint && included map_contextualizer && enhancements on nav_action_service

--- ws/src/actions/scripts/navServer.py
+++ ws/src/actions/scripts/navServer.py
@@ -17,7 +17,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actions.msg
 from actions.msg import navServAction, navServGoal, navServResult
 
-BASE_PATH = str(pathlib.Path(__file__).parent) + '/../../../../'
+BASE_PATH = str(pathlib.Path(__file__).parent) + "/../../map_contextualizer"
 
 placesPoses = {
  "BATHROOM": Pose(Point(x=-0.3963, y=3.802, z=0.0), Quaternion(x=0.0, y=0.0, z=0.4664, w=0.8845)),
@@ -26,6 +26,7 @@ placesPoses = {
  "KITCHEN": Pose(Point(x=-1.256, y=1.5809, z=0.0), Quaternion(x=0.0, y=0.0, z=-0.9992, w=0.0392)),
  "HOME": Pose(Point(x=-4.344, y=0.579, z=0.0), Quaternion(x=0.0, y=0.0, z=0.679, w=0.733)),
 }
+
 
 class navigationServer(object):
 
@@ -38,6 +39,8 @@ class navigationServer(object):
         self.move_client.wait_for_server()
         rospy.loginfo("MoveBase AS Loaded ...")
 
+        self.initPlaces()
+
         # Localization
         #rospy.loginfo("Global Localization")
         #rospy.wait_for_service('/global_localization')
@@ -49,10 +52,48 @@ class navigationServer(object):
         self._as = actionlib.SimpleActionServer(self._action_name, actions.msg.navServAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
     
+
+    def initPlaces(self):
+        # create a dictionary of places and their poses using the json file which has the following format
+        # key : {subkey_1 : pose1, subkey_2 : pose2, ...}
+        # where pose is a list of 6 elements [x, y, z, qx, qy, qz, qw]
+
+        self.placesPoses = {}
+        with open(BASE_PATH + '/areas.json') as json_file:
+            data = json.load(json_file)
+            for key in data:
+                self.placesPoses[key] = {}
+                for subkey in data[key]:
+                    self.placesPoses[key][subkey] = Pose(
+                                                        Point(
+                                                            x=data[key][subkey][0], 
+                                                            y=data[key][subkey][1], 
+                                                            z=data[key][subkey][2]), 
+                                                        Quaternion(
+                                                            x=data[key][subkey][3], 
+                                                            y=data[key][subkey][4], 
+                                                            z=data[key][subkey][5], 
+                                                            w=data[key][subkey][6]))
+                    
+                    rospy.loginfo(f"Loaded {key} {subkey} at {data[key][subkey]}")
+
+    
     def execute_cb(self, goal):
-        target = goal.target_location
+        target = str(goal.target_location)
+
+        if target == "":
+            return
+
+        keys = target.split(" ")
+
+        print(keys)
+    
+        if (len(keys) <= 1):
+            self.send_goal(self.placesPoses[keys[0]]['safe_place'])
+        else:
+            self.send_goal(self.placesPoses[keys[0]][keys[1]])
+
         rospy.loginfo("Robot Moving Towards " + target)
-        self.send_goal(placesPoses[target])
         self._as.set_succeeded(navServResult(result=True))
 
     def send_goal(self, target_pose):
