c8a221f59867092fbd314efb5c3772f5886a18b6
Chapa-1810 <alexischaparomero@gmail.com>
2024-07-01 21:27:33 -0600

    approach table

--- ws/src/actions/scripts/moveServer.py
+++ ws/src/actions/scripts/moveServer.py
@@ -13,8 +13,8 @@ class MoveServer(object):
     def __init__(self):
         self.scan_data = None
         self.robot_pose = None
-        self.max_range = 0.8
-        self.min_range = 0.40
+        self.target_departure = 0.6
+        self.target_approach = 0.25
         self.x_vel = 0.05
         self.angular_vel = 0.05
         self.success = True
@@ -25,6 +25,7 @@ class MoveServer(object):
         self.scan_sub = rospy.Subscriber('/oakd/scan', LaserScan, self.scan_callback, queue_size=1)
         self.robot_pose_sub = rospy.Subscriber('/robot_pose', PoseStamped, self.robot_pose_callback, queue_size=1)
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        
         rospy.loginfo('Move server initialized')
 
     def execute(self, goal):
@@ -40,7 +41,7 @@ class MoveServer(object):
         self.end_index = int(((math.pi / 4) - self.scan_data.angle_min) / self.scan_data.angle_increment) + 1
         cmd_vel = Twist()
 
-        if goal.goal_type == moveActionGoal.APROACH:
+        if goal.goal_type == moveActionGoal.FORWARD:
             self.move_approach(cmd_vel=cmd_vel, goal=goal)
         elif goal.goal_type == moveActionGoal.BACKWARD:
             self.move_backward(cmd_vel)
@@ -65,73 +66,11 @@ class MoveServer(object):
         return
 
     def move_approach(self, cmd_vel : Twist, goal : PoseStamped):
-        tf_broadcaster = tf2_ros.TransformBroadcaster()
-        
-        tf = TransformStamped()
-        tf.header.stamp = rospy.Time.now()
-        tf.header.frame_id = 'map'
-        tf.child_frame_id = 'goal'
-        tf.transform.translation.x = goal.pose.position.x
-        tf.transform.translation.y = goal.pose.position.y
-        tf.transform.translation.z = 0
-        tf.transform.rotation.x = goal.pose.orientation.x
-        tf.transform.rotation.y = goal.pose.orientation.y
-        tf.transform.rotation.z = goal.pose.orientation.z
-        tf.transform.rotation.w = goal.pose.orientation.w
-
-        tf_broadcaster.sendTransform(tf)
-
-        try: 
-            self.tf_buffer.lookup_transform('map', 'goal', rospy.Time())
-        except Exception as e:
-            print(e)
-            rospy.logerr("error on transformation lookup")
-            self.r.sleep()
-            return
-        
-        try:
-            robot_from_table = self.tf_buffer.transform(self.robot_pose, 'goal', rospy.Duration(1))
-        except Exception as e:
-            print(e)
-            rospy.logerr("Failed to transform object pose from body_frame to map_frame")
-            self.r.sleep()
-            return
-
-        # Get transformation matrix between goal poseStamped and robot poseStamped
-        medium_pose_table = PoseStamped()
-        medium_pose_table.header.stamp = rospy.Time.now()
-        medium_pose_table.header.frame_id = 'map'
-        medium_pose_table.pose.position.x = robot_from_table.pose.position.x
-        medium_pose_table.pose.position.y = robot_from_table.pose.position.y * 0.5
-        medium_pose_table.pose.position.z = 0
-        medium_pose_table.pose.orientation.x = 0
-        medium_pose_table.pose.orientation.y = 0
-        medium_pose_table.pose.orientation.z = 0
-        medium_pose_table.pose.orientation.w = 1
-
-        try:
-            medium_pose = self.tf_buffer.transform(medium_pose_table, 'map', rospy.Duration(1))
-        except Exception as e:
-            print(e)
-            rospy.logerr("Failed to transform object pose from body_frame to map_frame")
-            self.r.sleep()
-            return
-
-        path = []
-        path.append(medium_pose)
-        path.append(goal)
-
-        solve_angle = True
-        solve_approach = True
-        distance = float('inf')
-        goal_map = PoseStamped()
-
-        while len(path) > 0 and solve_angle and solve_approach:
-            if solve_angle and solve_approach:
-                goal_map = path.pop(0)
-                solve_angle = False
-                solve_approach = False
-
+        goal_pose = self.tf_buffer.transform(goal, 'base_footprint', rospy.Duration(0.1))
+        min_scanned_distance = float('inf')
+        solve_angle = False
+        solve_approach = False
+        while min_scanned_distance > self.target_approach:
             if self.server.is_preempt_requested() or rospy.is_shutdown():
                 rospy.loginfo('Preempted')
                 self.server.set_preeumpted()
@@ -139,21 +78,31 @@ class MoveServer(object):
                 self.server.set_aborted()
                 return
             
+            curr_scanned_distance = float('inf') 
+
+            for i in range(self.init_index, self.end_index):
+                if self.scan_data.ranges[i] == float('inf'):
+                    continue
+                curr_scanned_distance = min(curr_scanned_distance, self.scan_data.ranges[i])
+            
+            rospy.loginfo('Scanned distance: {}'.format(curr_scanned_distance))
+
             if not solve_angle:
-                angle = math.atan2(goal_map.pose.position.y - self.robot_pose.pose.position.y, goal_map.pose.position.x - self.robot_pose.pose.position.x)
+                angle = math.atan2(goal_pose.pose.position.y - self.robot_pose.pose.position.y, goal_pose.pose.position.x - self.robot_pose.pose.position.x)
                 
-                if abs(angle - self.robot_pose.pose.orientation.z) > 0.1:
+                if abs(angle - self.robot_pose.pose.orientation.z) > 5:
                     cmd_vel.angular.z = self.angular_vel if angle > self.robot_pose.pose.orientation.z else -self.angular_vel
                 else:
                     cmd_vel.angular.z = 0
                     solve_angle = True
             elif not solve_approach:
-                distance = math.sqrt((goal_map.pose.position.x - self.robot_pose.pose.position.x) ** 2 + (goal_map.pose.position.y - self.robot_pose.pose.position.y) ** 2)
+                distance = math.sqrt((goal_pose.pose.position.x - self.robot_pose.pose.position.x) ** 2 + (goal_pose.pose.position.y - self.robot_pose.pose.position.y) ** 2)
                 if distance > 0.05:
                     cmd_vel.linear.x = self.x_vel
                 else:
                     cmd_vel.linear.x = 0
                     solve_approach = True
+
 
             if not DEBUG:
                 self.cmd_vel_pub.publish(cmd_vel)
@@ -162,10 +111,10 @@ class MoveServer(object):
     
     # REDO THIS FUNCTION
     def move_backward(self, cmd_vel):
-        min_distance = self.min_range
+        min_scanned_distance = self.min_range
 
         while True:
-            min_distance = float('inf')
+            min_scanned_distance = float('inf')
             
             if self.server.is_preempt_requested() or rospy.is_shutdown():
                 rospy.loginfo('Preempted')
@@ -182,13 +131,13 @@ class MoveServer(object):
                 if self.scan_data.ranges[i] == float('inf'):
                     continue
 
-                min_distance = min(min_distance, self.scan_data.ranges[i])
+                min_scanned_distance = min(min_scanned_distance, self.scan_data.ranges[i])
             
-            rospy.loginfo('Max distance: {}'.format(min_distance))
+            rospy.loginfo('Max distance: {}'.format(min_scanned_distance))
             #self.server.publish_feedback('Max distance: {}'.format(max_distance))
             self.r.sleep()
 
-            if min_distance >= self.max_range:
+            if min_scanned_distance >= self.max_range:
                 break
         
         #self.server.publish_feedback("Achieved max distance")
