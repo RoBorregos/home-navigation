7d13de43de077fdfadb145d0b3ae01e0155801da
GerardoFJ <A00838645@tec.mx>
2024-10-17 20:44:37 -0600

    Added workspace and Dockerfile v1

---
+++ ws/src/my_package/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gerardo',
+    maintainer_email='A00838645@tec.mx',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_package.my_node:main',
+            'dashgo_driver = my_package.dashgo_driver:main'
+        ],
+    },
+)
