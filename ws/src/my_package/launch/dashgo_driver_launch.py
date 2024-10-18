1752f509a016f979b457ab47da9201f2eb358098
GerardoFJ <A00838645@tec.mx>
2024-10-18 00:56:40 -0600

    edit setup and launch

--- ws/src/my_package/launch/dashgo_driver_launch.py
+++ ws/src/my_package/launch/dashgo_driver_launch.py
@@ -8,24 +8,24 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     # File paths
-    dashgo_driver_config_path = os.path.join(get_package_share_directory('dashgo_driver'), 'config', 'my_dashgo_params.yaml')
+    dashgo_driver_config_path = os.path.join(get_package_share_directory('my_package'), 'config', 'my_dashgo_params.yaml')
     # yocs_velocity_smoother_config_path = os.path.join(get_package_share_directory('dashgo_driver'), 'config', 'yocs_velocity_smoother.yaml')
     # velocity_smoother_launch_path = os.path.join(get_package_share_directory('yocs_velocity_smoother'), 'launch', 'velocity_smoother.launch.py')
 
     return LaunchDescription([
         # Declare launch arguments
-        DeclareLaunchArgument('node_name', default_value='velocity_smoother'),
-        DeclareLaunchArgument('nodelet_manager_name', default_value='nodelet_manager'),
-        # DeclareLaunchArgument('config_file', default_value=yocs_velocity_smoother_config_path),
-        DeclareLaunchArgument('raw_cmd_vel_topic', default_value='cmd_vel'),
-        DeclareLaunchArgument('smooth_cmd_vel_topic', default_value='smoother_cmd_vel'),
-        DeclareLaunchArgument('robot_cmd_vel_topic', default_value='robot_cmd_vel'),
-        DeclareLaunchArgument('odom_topic', default_value='odom'),
+        # DeclareLaunchArgument('node_name', default_value='velocity_smoother'),
+        # DeclareLaunchArgument('nodelet_manager_name', default_value='nodelet_manager'),
+        # # DeclareLaunchArgument('config_file', default_value=yocs_velocity_smoother_config_path),
+        # DeclareLaunchArgument('raw_cmd_vel_topic', default_value='cmd_vel'),
+        # DeclareLaunchArgument('smooth_cmd_vel_topic', default_value='smoother_cmd_vel'),
+        # DeclareLaunchArgument('robot_cmd_vel_topic', default_value='robot_cmd_vel'),
+        # DeclareLaunchArgument('odom_topic', default_value='odom'),
 
         # Use simulation time parameter
         Node(
-            package='dashgo_driver',
-            executable='dashgo_driver.py',
+            package='my_package',
+            executable='dashgo_driver',
             name='dashgo_driver',
             output='screen',
             respawn=True,
@@ -55,28 +55,28 @@ def generate_launch_description():
         # ),
 
         # Static transform publishers for sonar
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_link_to_sonar0',
-            arguments=['0.18', '0.10', '0.115', '0.524', '0.0', '0.0', '/base_footprint', '/sonar0', '40'],
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_link_to_sonar1',
-            arguments=['0.20', '0.0', '0.115', '0.0', '0.0', '0.0', '/base_footprint', '/sonar1', '40'],
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_link_to_sonar2',
-            arguments=['0.18', '-0.10', '0.115', '-0.524', '0.0', '0.0', '/base_footprint', '/sonar2', '40'],
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_link_to_sonar3',
-            arguments=['-0.20', '0.0', '0.115', '3.14', '0.0', '0.0', '/base_footprint', '/sonar3', '40'],
-        ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='base_link_to_sonar0',
+        #     arguments=['0.18', '0.10', '0.115', '0.524', '0.0', '0.0', '/base_footprint', '/sonar0', '40'],
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='base_link_to_sonar1',
+        #     arguments=['0.20', '0.0', '0.115', '0.0', '0.0', '0.0', '/base_footprint', '/sonar1', '40'],
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='base_link_to_sonar2',
+        #     arguments=['0.18', '-0.10', '0.115', '-0.524', '0.0', '0.0', '/base_footprint', '/sonar2', '40'],
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='base_link_to_sonar3',
+        #     arguments=['-0.20', '0.0', '0.115', '3.14', '0.0', '0.0', '/base_footprint', '/sonar3', '40'],
+        # ),
     ])
