912125a5a1d3c5cc00bdc8433436d28f41854825
GerardoFJ <A00838645@tec.mx>
2024-10-18 00:42:32 -0600

    Edited setup py

--- ws/src/my_package/setup.py
+++ ws/src/my_package/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+from glob import glob
+import os
 package_name = 'my_package'
 
 setup(
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
