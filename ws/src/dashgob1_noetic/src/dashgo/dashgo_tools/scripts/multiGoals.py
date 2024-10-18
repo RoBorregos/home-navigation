d3c89b9a908f72a61651729b5e1365d9ca2ce34d
GerardoFJ <A00838645@tec.mx>
2024-10-17 20:42:42 -0600

    Rebased Branch

--- ws/src/dashgob1_noetic/src/dashgo/dashgo_tools/scripts/multiGoals.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#coding=utf-8  
-import rospy
-from pyc import MultiGoals
-
-if __name__ == '__main__':
-  MultiGoals.MultiGoalListen()
-
-
--- ws/src/yocs_velocity_smoother/README.md
+++
@@ -1,3 +0,0 @@
-# yocs_velocity_smoother
-
-This ROS Package is based on the work at https://github.com/yujinrobot/yujin_ocs
