e79173fbe257bc8bdfb772efd158af2fc792ad7c
GerardoFJ <A00838645@tec.mx>
2024-10-25 00:11:06 -0600

    Updated params and launch

---
+++ ws/src/my_package/launch/dashgo_driver.launch.py
@@ -0,0 +1,24 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = os.path.join(
+      get_package_share_directory('my_package'),
+      'config',
+      'dashgo_params.yaml'
+      )
+    return LaunchDescription([
+        Node(
+            package='my_package',
+            executable='my_node',
+            name='DashgoDriver',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                # {'my_parameter': 'test'},
+                config
+            ],
+        ),
+    ])
