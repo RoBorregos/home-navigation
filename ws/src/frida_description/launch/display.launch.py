628e6a1a28454c325b92a7e8824264065b1c94a0
AleGonzcamilla <A00839233@tec.mx>
2024-12-19 21:22:09 -0600

    minor changes to urdf

--- ws/src/frida_description/launch/display.launch.py
+++ ws/src/frida_description/launch/display.launch.py
@@ -1,10 +1,9 @@
-
+ #!/usr/bin/env python3
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -13,6 +12,6 @@ def generate_launch_description():
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
         launch_arguments={
             'urdf_package': 'frida_description',
-            'urdf_package_path': PathJoinSubstitution(['urdf','FRIDAurdf','urdf','FRIDA.urdf.xacro'])}.items()
+            'urdf_package_path': PathJoinSubstitution(['urdf','urdf','FRIDA.urdf'])}.items()
     ))
     return ld
